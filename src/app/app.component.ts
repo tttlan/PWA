60ea0c8108fb387852f2496e117539fd6247f949
lantran <tttlan1503code@gmail.com>
2018-10-26 16:55:43 +0700

    dddd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { DataService } from './data.service';
+import * as javascriptBarcodeReader from 'javascript-barcode-reader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
 
   update: boolean = false;
   joke: any;
+  code: any;
 
   constructor(updates: SwUpdate, private data: DataService) {
-    console.log("ehllo 00000");
-    updates.available.subscribe(event => {
-      console.log("ehllo");
-      //this.update = true;
-      updates.activateUpdate().then(() => document.location.reload());
-
-    })
+    // updates.available.subscribe(event => {
+    //   updates.activateUpdate().then(() => document.location.reload());
+    // });
   }
 
   ngOnInit() {
-    this.data.gimmeJokes().subscribe(res => {
-      this.joke = res;
-    })
+    // this.data.gimmeJokes().subscribe(res => {
+    //   this.joke = res;
+    // });
+
+    javascriptBarcodeReader(
+      Image /* Image file Path || {data: pixelArray, width, height} || HTML5 Canvas ImageData */,
+      {
+        barcode: 'Codabar',
+        type: 'industrial',
+      }
+    ).then(code => {
+      this.code = code;
+    });
   }
 
 }
